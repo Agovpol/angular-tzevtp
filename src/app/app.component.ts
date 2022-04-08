67142a6cdcc0bed93f22feae6a94cdc6c4c2e591
Agovpol <agovpol@gmail.com>
2022-04-08 18:53:00 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { hasGlobalAnalyticsConfiguration } from '@angular/cli/models/analytics';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
